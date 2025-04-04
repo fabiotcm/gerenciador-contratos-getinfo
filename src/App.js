2761be0bad69bf50cf7f8f363291c143fec46821
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-04-04 11:14:39 -0300

    [ADD] Router-dom e página cadastro

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import CadastrarCliente from './components/CadastrarCliente';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import Cadastro from './pages/Cadastro';
 
 
 function App() {
   return (
-    <div className="wrapper">
-      <Navbar />
-      <Sidebar />
-
-      {/* Conteúdo principal da sua aplicação */}
-      <div className="content-wrapper">
-      <CadastrarCliente/>
-        <section className="content">
-          <div className="container-fluid">
-            {/* Seu conteúdo aqui */}
-          </div>
-        </section>
-      </div>
-
-      {/* Footer e outros elementos do AdminLTE */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' Component={Cadastro}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
