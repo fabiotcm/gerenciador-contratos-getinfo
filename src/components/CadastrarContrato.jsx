63ecfea2a4cfe4a5add7a89958fa1ba4d347f164
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-04-04 12:04:10 -0300

    Adicionei os casos de uso de cadastar contrato

--- src/components/CadastrarContrato.jsx
+++ src/components/CadastrarContrato.jsx
@@ -7,7 +7,7 @@ const CadastrarContrato = () => {
     <div className="col-md-6">
       <div className="card card-primary">
         <div className="card-header">
-          <h3 className="card-title">Cadastrar Cliente</h3>
+          <h3 className="card-title">Cadastrar Contrato</h3>
           <div className="card-tools">
             <button type="button" className="btn btn-tool" data-card-widget="collapse" title="Collapse">
               <i className="fas fa-minus" />
@@ -16,22 +16,68 @@ const CadastrarContrato = () => {
         </div>
         <div className="card-body">
           <div className="form-group">
-            <label htmlFor="inputName">CNPJ</label>
-            <input type="number" id="inputName" className="form-control" />
+            <label htmlFor="inputName">Preposto(responsável pelo contrato)</label>
+            <input type="text" id="inputName" className="form-control" />
           </div>
           <div className="form-group">
-            <label htmlFor="inputDescription">Endereço</label>
+            <label htmlFor="inputName">Nome da empresa</label>
+            <input type="text" id="inputName" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputDescription">Endereço da empresa</label>
             <textarea id="inputDescription" className="form-control" rows={4} defaultValue={""} />
           </div>
           <div className="form-group">
+            <label htmlFor="inputName">Nome do principal contato da empresa contratante</label>
+            <input type="text" id="inputName" className="form-control" />
           </div>
           <div className="form-group">
-            <label htmlFor="inputClientCompany">Número para contato</label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputClientCompany">Telefone do principal contato da empresa contratante</label>
             <input type="tel" id="inputClientCompany" className="form-control" />
           </div>
           <div className="form-group">
-            <label htmlFor="inputProjectLeader">Email</label>
-            <input type="email" id="inputProjectLeader" className="form-control" />
+            <label htmlFor="inputProjectLeader">CNPJ</label>
+            <input type="number" id="inputProjectLeader" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputName">Custo</label>
+            <input type="text" id="inputName" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputName">Anexo do documento(contrato)</label>
+            <input type="file" id="inputName" className="form-control" />
+          </div>
+          <div class="form-group">
+                <label for="inputStatus">Tipo de Contrato</label>
+                <select id="inputStatus" class="form-control custom-select">
+                  <option selected disabled>Tipo</option>
+                  <option>Posto de Serviço</option>
+                  <option>Serviço</option>
+                  <option>Comunicação</option>
+                  <option>Infraestrutura</option>
+                  <option>Desenvolvimento</option>
+                </select>
+          </div>
+          <div className="form-group">Prazo de Vigência
+            <p/>
+            <label htmlFor="inputName">Data de início</label>
+            <input type="date" id="inputName" className="form-control" />
+            <label htmlFor="inputName">Data do fim</label>
+            <input type="date" id="inputName" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputDescription">Entregáveis</label>
+            <textarea id="inputDescription" className="form-control" rows={4} defaultValue={""} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputDescription">Postos de serviço</label>
+            <textarea id="inputDescription" className="form-control" rows={4} defaultValue={""} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="inputDescription">Descrição</label>
+            <textarea id="inputDescription" className="form-control" rows={4} defaultValue={""} />
           </div>
         </div>
         {/* /.card-body */}
