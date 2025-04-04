71e0a40e10f8f1c4c895ec451cd579911825d009
Fábio Cardoso <fabiotarcisioccpa@gmail.com>
2025-04-04 16:05:05 -0300

    [ADD] Logo getinfo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import logo from "../images/logo_getinfo.png";
+
 
 const Sidebar = () => {
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       {/* Logo */}
-      <a href="../../index3.html" className="brand-link">
-        <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-        <span className="brand-text font-weight-light">AdminLTE 3</span>
+      <a className="brand-link">
+        <img src={logo} alt="GetInfo logo" className="brand-image" />
+        <span>GetInfo</span>
       </a>
 
       {/* Sidebar */}
